70cd63f8751e2299b30d0e2746096f4c7f748a98
soichiro-nitta <soic1226@gmail.com>
2019-10-06 17:25:13 +0900

    Update humberger

--- src/components/default/Humberger.tsx
+++ src/components/default/Humberger.tsx
@@ -2,9 +2,31 @@ import * as React from 'react'
 import styled from '~/utils/emotion'
 
 const Humberger: React.FC = () => {
-  return <Root>Humberger component</Root>
+  return (
+    <Root>
+      <Border1 />
+      <Border2 />
+      <Border3 />
+    </Root>
+  )
 }
 
-const Root = styled.div``
+const Root = styled.div`
+  margin-top: 3px;
+  width: 20px;
+`
+const Border1 = styled.div`
+  width: 100%;
+  height: 2px;
+  background: ${(props): string => props.theme.colors.light.logo};
+`
+const Border2 = styled(Border1)`
+  margin-top: 4px;
+  width: 75%;
+`
+const Border3 = styled(Border1)`
+  margin-top: 4px;
+  width: 90%;
+`
 
 export default Humberger
