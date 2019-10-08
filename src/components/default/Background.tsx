3e1835dd93dfeeee806eea7c499f9d37bfe4f541
soichiro-nitta <soic1226@gmail.com>
2019-10-08 13:37:28 +0900

    Fix style

--- src/components/default/Background.tsx
+++ src/components/default/Background.tsx
@@ -3,59 +3,81 @@ import styled from '~/utils/emotion'
 
 const Background: React.FC = () => (
   <Root>
-    <Border1 />
-    <Border2 />
+    <Border1>
+      <Relative>
+        <Border1A />
+        <Border1B />
+      </Relative>
+    </Border1>
+    <Border2>
+      <Relative>
+        <Border2A />
+        <Border2B />
+      </Relative>
+    </Border2>
     <Border3 />
     <Border4 />
-    <Border5 />
-    <Border6 />
   </Root>
 )
 
-const Root = styled.h1`
+const Root = styled.div`
   width: 100%;
   height: 100%;
+`
+const Relative = styled.div`
+  ${(props): string => props.theme.mixins.relative}
 `
 const Border1 = styled.div`
   position: absolute;
   top: 0;
   left: ${(props): number => props.theme.sizes.phone.dashboard}px;
   width: 1px;
+  height: 100%;
+`
+const Border1A = styled.div`
+  width: 100%;
   height: 9px;
   background: ${(props): string => props.theme.colors.light.border};
 `
-const Border2 = styled(Border1)`
+const Border1B = styled.div`
+  position: absolute;
   top: ${(props): number => props.theme.sizes.phone.dashboard}px;
+  width: 100%;
   height: calc(
     100% - ${(props): number => props.theme.sizes.phone.dashboard}px
   );
+  background: ${(props): string => props.theme.colors.light.border};
 `
-const Border3 = styled(Border1)`
+const Border2 = styled(Border1)`
   left: calc(
-    (100% - ${(props): number => props.theme.sizes.phone.dashboard * 2 + 2}px) /
-      3 + ${(props): number => props.theme.sizes.phone.dashboard + 1}px
+    (
+        (
+            100% -
+              ${(props): number => props.theme.sizes.phone.dashboard * 2 + 2}px
+          ) - 2px
+      ) / 3 + ${(props): number => props.theme.sizes.phone.dashboard + 1}px
   );
+`
+const Border2A = styled(Border1A)`
   height: 11px;
 `
-const Border4 = styled(Border1)`
-  top: ${(props): number => props.theme.sizes.phone.dashboard}px;
-  left: calc(
-    (100% - ${(props): number => props.theme.sizes.phone.dashboard * 2 + 2}px) /
-      3 + ${(props): number => props.theme.sizes.phone.dashboard + 1}px
+const Border2B = Border1B
+const Border3 = styled.div`
+  position: absolute;
+  top: 0;
+  right: calc(
+    (
+        (
+            100% -
+              ${(props): number => props.theme.sizes.phone.dashboard * 2 + 2}px
+          ) - 2px
+      ) / 3 + ${(props): number => props.theme.sizes.phone.dashboard + 1}px
   );
-  height: calc(
-    100% - ${(props): number => props.theme.sizes.phone.dashboard}px
-  );
-`
-const Border5 = styled(Border1)`
-  left: calc(
-    (100% - ${(props): number => props.theme.sizes.phone.dashboard * 2 + 2}px) /
-      3 * 2 + ${(props): number => props.theme.sizes.phone.dashboard + 1}px
-  );
+  width: 1px;
   height: 100%;
+  background: ${(props): string => props.theme.colors.light.border};
 `
-const Border6 = styled(Border1)`
-  left: auto;
+const Border4 = styled(Border3)`
   right: ${(props): number => props.theme.sizes.phone.dashboard}px;
   height: 100%;
 `
