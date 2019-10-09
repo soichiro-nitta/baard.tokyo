9a4454167f5e997e3b270b9c2f97ffad52ced0f0
soichiro-nitta <soic1226@gmail.com>
2019-10-09 23:33:24 +0900

    add hex2rgb function

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -46,6 +46,23 @@ const functions = {
     video.currentTime = Math.floor(Math.random() * (video.duration + 1 - 0))
     await this.$canplayVideo(video)
     video.play()
+  },
+  hex2rgb(hex: string): number[] {
+    if (hex.slice(0, 1) == '#') hex = hex.slice(1)
+    if (hex.length == 3)
+      hex =
+        hex.slice(0, 1) +
+        hex.slice(0, 1) +
+        hex.slice(1, 2) +
+        hex.slice(1, 2) +
+        hex.slice(2, 3) +
+        hex.slice(2, 3)
+    // TODO: ここを文字列で返すようにする
+    return [hex.slice(0, 2), hex.slice(2, 4), hex.slice(4, 6)].map(function(
+      str
+    ) {
+      return parseInt(str, 16)
+    })
   }
 }
 
