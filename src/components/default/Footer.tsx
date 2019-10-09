c48120c21167861f68ca673411e1ed3645689222
soichiro-nitta <soic1226@gmail.com>
2019-10-10 04:52:18 +0900

    Add sns icons on footer

--- src/components/default/Footer.tsx
+++ src/components/default/Footer.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebookSquare,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
+import config from '~/utils/config'
 import styled from '~/utils/emotion'
 
 const Humberger: React.FC = () => {
   return (
     <Root>
-      <Footer>© {new Date().getFullYear()}, BAARD Barber.</Footer>
+      <Footer>
+        <Text>© {new Date().getFullYear()}, BAARD Barber.</Text>
+        <Icons>
+          <Icon
+            href={config.facebook}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebookSquare} />
+          </Icon>
+          <Icon
+            href={config.instagram}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </Icon>
+        </Icons>
+      </Footer>
     </Root>
   )
 }
@@ -14,6 +38,9 @@ const Root = styled.div`
   border-bottom: 1px solid ${(props): string => props.theme.colors.light.border};
 `
 const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin: ${(props): number =>
       (props.theme.sizes.phone.dashboard - props.theme.sizes.phone.scrollbar) /
       2}px
@@ -31,6 +58,14 @@ const Footer = styled.div`
   color: white;
   line-height: 1;
   background: ${(props): string => props.theme.colors.light.shape};
+`
+const Text = styled.div``
+const Icons = styled.div``
+const Icon = styled.a`
+  margin-left: ${(props): number =>
+    (props.theme.sizes.phone.dashboard - props.theme.sizes.phone.scrollbar) /
+    2}px;
+  font-size: 16px;
 `
 
 export default Humberger
