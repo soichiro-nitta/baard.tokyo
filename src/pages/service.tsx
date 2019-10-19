2cdadd9d2a92060d012f13e3c85b202f10c97bf3
soichiro-nitta <soic1226@gmail.com>
2019-10-19 09:59:19 +0900

    Use service.png

--- src/pages/service.tsx
+++ src/pages/service.tsx
@@ -1,74 +1,85 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
-import config from '~/utils/config'
+import { faClipboardList } from '@fortawesome/pro-duotone-svg-icons'
 import styles from '~/utils/styles'
 import useMain from '~/hooks/base/useMain'
+import useFluid from '~/hooks/service/useFluid'
 import Seo from '~/components/base/Seo'
+import HeaderImage from '~/components/base/HeaderImage'
+import H2 from '~/components/base/H2'
 import ButtonNew from '~/components/base/ButtonNew'
+import Row from '~/components/base/Row'
+import Map from '~/components/access/Map'
+import Table from '~/components/access/Table'
 
 const Service: React.FC = () => {
   useMain()
+  const fluid = useFluid()
   return (
-    <Root>
+    <>
       <Seo title="Service" />
-      <P1>
-        Serviceページです。亀有にオープンしたBAARD理髪店のウェブサイトです。
-        ここに軽めの序文を。ここに軽めの序文を。ここに軽めの序文を。
-      </P1>
-      <ButtonWrapper>
-        <ButtonNew to={config.instagram} text="スタイル集はInstagramにて" />
-      </ButtonWrapper>
+      <HeaderImage
+        fluid={fluid}
+        icon={faClipboardList}
+        en="SERVICE"
+        ja="メニュー案内"
+        crumbs={[
+          {
+            to: '/',
+            name: 'ホーム'
+          },
+          {
+            to: '/service',
+            name: 'メニュー案内'
+          }
+        ]}
+      />
       <Wrapper>
-        <P2>
-          Facebookページにてお店の情報やお知らせ、近況のご報告などをおこなっています。
-        </P2>
-        <ButtonWrapper>
-          <ButtonNew to={config.facebook} text="近況報告はFacebookにて" />
-        </ButtonWrapper>
+        <H2 en="MAP" ja="マップ" />
       </Wrapper>
       <Wrapper>
-        <P2>
-          <b>店名</b>
-        </P2>
-        <P2>BAARD（バーールト）理髪店</P2>
+        <ButtonNew
+          to="https://www.google.com/maps/search/?api=1&query=BAARD理髪店"
+          text="Google Mapsでみる"
+        />
       </Wrapper>
       <Wrapper>
-        <P2>
-          <b>所在地</b>
-        </P2>
-        <P2>〒125-0061 東京都葛飾区亀有 2-59-7</P2>
+        <Row head="店名" data="BAARD（バーールト）理髪店" />
       </Wrapper>
       <Wrapper>
-        <P2>
-          <b>電話番号</b>
-        </P2>
-        <P2>03-0000-0000</P2>
+        <Row head="住所" data="〒125-0061 東京都葛飾区亀有 2-59-7" />
       </Wrapper>
-    </Root>
+      <Wrapper>
+        <Row head="最寄駅" data="JR常磐線 亀有駅 徒歩8分" />
+      </Wrapper>
+      <Wrapper>
+        <Row head="電話番号" data="03-0000-0000" />
+      </Wrapper>
+      <Wrapper>
+        <Row head="メールアドレス" data="baard-tokyo@gmail.com" />
+      </Wrapper>
+      <MapWrapper>
+        <Map />
+      </MapWrapper>
+      <Wrapper>
+        <H2 en="OPEN" ja="営業時間" />
+      </Wrapper>
+      <Wrapper>
+        <Table />
+      </Wrapper>
+    </>
   )
 }
 
-const Root = styled.div`
-  margin-top: ${styles.sizes.phone.dashboard +
-    (styles.sizes.phone.dashboard - 30) / 2 +
-    5}px;
-`
 const Wrapper = styled.div`
   margin-top: ${styles.sizes.phone.base}px;
+  padding-top: ${styles.sizes.phone.base}px;
+  padding-right: ${styles.sizes.phone.base}px;
+  padding-left: ${styles.sizes.phone.base}px;
   border-top: 1px solid ${styles.colors.light.border};
 `
-const P1 = styled.p`
-  padding: 0 ${styles.sizes.phone.base}px;
-  width: 100%;
-  ${styles.mixins.lhCrop(2)}
-`
-const P2 = styled(P1)`
-  margin-top: ${styles.sizes.phone.base}px;
-`
-const ButtonWrapper = styled.div`
-  margin-top: ${(styles.sizes.phone.dashboard - styles.sizes.phone.scrollbar) /
-    3}px;
-  margin-left: ${styles.sizes.phone.base}px;
+const MapWrapper = styled(Wrapper)`
+  padding-left: 0;
 `
 
 export default Service
