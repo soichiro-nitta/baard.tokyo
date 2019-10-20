9908a6f01d51c6177e74830935163aa073814224
Soichiro Nitta <soic1226@gmail.com>
2019-10-20 14:42:03 +0900

    Salonページいったんできた (#16)
    * Update access page
    
    * Add images
    
    * Update usefluid hook for salon page
    
    * Update salon page

--- src/hooks/salon/useFluid.tsx
+++ src/hooks/salon/useFluid.tsx
@@ -11,9 +11,43 @@ const useFluid = (): FluidObject | FluidObject[] => {
           }
         }
       }
+      salon1: file(relativePath: { eq: "salon1.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      salon2: file(relativePath: { eq: "salon2.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      shun: file(relativePath: { eq: "shun.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      risa: file(relativePath: { eq: "risa.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
     }
   `)
-  return data.salon.childImageSharp.fluid
+  return [
+    data.salon.childImageSharp.fluid,
+    data.salon1.childImageSharp.fluid,
+    data.salon2.childImageSharp.fluid,
+    data.shun.childImageSharp.fluid,
+    data.risa.childImageSharp.fluid
+  ]
 }
 
 export default useFluid
