4d0946072257aa832e51d6d5d33097d2ecadd3d8
soichiro-nitta <soic1226@gmail.com>
2019-10-20 14:38:47 +0900

    Update salon page

--- src/pages/salon.tsx
+++ src/pages/salon.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Img from 'gatsby-image'
 import { faCut } from '@fortawesome/pro-duotone-svg-icons'
 import styled from '@emotion/styled'
 import styles from '~/utils/styles'
@@ -10,7 +11,7 @@ import HeaderImage from '~/components/base/HeaderImage'
 import Wrapper from '~/components/base/Wrapper'
 import Br from '~/components/base/Br'
 import H2 from '~/components/base/H2'
-import List from '~/components/service/List'
+import Exhibition from '~/components/base/Exhibition'
 
 const Salon: React.FC = () => {
   useMain()
@@ -37,47 +38,42 @@ const Salon: React.FC = () => {
         ]}
       />
       <Wrapper>
-        <H2 en="BARBER" ja="メンズメニュー" />
+        <H2 en="CONCEPT" ja="サロンコンセプト" />
       </Wrapper>
       <Wrapper>
         <P>
-          BAARDではヘアスタイルを作る上で３つの要素を大切に、ヘアデザインに落とし込みます。お客様の髪質に合わせ－－。ここに説明など。ここに説明など。ここに説明など。ここに説明など。ここに説明など。
+          サロンコンセプト。BAARDでは－－。ここに説明など。ここに説明など。ここに説明など。ここに説明など。ここに説明など。
         </P>
       </Wrapper>
+      <Exhibition>
+        <ExhibitionImg>
+          <Img fluid={fluid[1]} />
+          <ExhibitionFilter />
+        </ExhibitionImg>
+      </Exhibition>
       <Wrapper>
-        <List head="メンズカット" price="5,000" />
-      </Wrapper>
-      <Wrapper>
-        <List head="前髪カット" price="1,000" />
-      </Wrapper>
-      <Wrapper>
-        <List head="キッズカット" price="3,000" />
-      </Wrapper>
-      <Br />
-      <Wrapper>
-        <List head="リタッチカラー" price="9,500" />
-      </Wrapper>
-      <Wrapper>
-        <List head="オールカラー" price="13,500" />
-      </Wrapper>
-      <Wrapper>
-        <List head="デザインカラー" price="6,000" />
-      </Wrapper>
-      <Wrapper>
-        <List head="ブリーチカラー" price="10,000" />
+        <P>↑ ここはスライダー（ギャラリー？）にします。鋭意制作中。</P>
       </Wrapper>
       <Br />
       <Wrapper>
-        <List head="コスメパーマ" price="8,500" />
+        <H2 en="RESERVATION" ja="ご予約について" />
       </Wrapper>
       <Wrapper>
-        <List head="エアウェーブパーマ" price="11,500" />
+        <P>
+          予約システムについて。BAARDでは初回のご来店以降のお客様に－－。ここに説明など。ここに説明など。ここに説明など。
+        </P>
       </Wrapper>
+      <Exhibition>
+        <ExhibitionImg>
+          <Img fluid={fluid[2]} />
+          <ExhibitionFilter />
+        </ExhibitionImg>
+      </Exhibition>
       <Wrapper>
-        <List head="デジタルパーマ" price="12,500" />
-      </Wrapper>
-      <Wrapper>
-        <List head="ポイントパーマ" price="3,000" />
+        <P>
+          ↑
+          予約システムについて写真が必要であれば。複数枚必要があれば、こちらもスライダーに。
+        </P>
       </Wrapper>
       <Br />
       <Wrapper>
@@ -85,11 +81,41 @@ const Salon: React.FC = () => {
       </Wrapper>
       <Wrapper>
         <P>
-          BAARDのレディースシェービングは－－。お顔のうぶ毛を剃ることにより－－。ここに説明など。ここに説明など。ここに説明など。ここに説明など。ここに説明など。
+          BAARDでは女性のお客様に－－。ここにもレディースシェービングについて紹介があるとわかりやすいかも？ここに説明など。
         </P>
       </Wrapper>
+      <Br />
       <Wrapper>
-        <List head="シェービング" price="3,000" />
+        <H2 en="STAFF" ja="スタッフのご紹介" />
+      </Wrapper>
+      <Exhibition>
+        <ExhibitionStaff>
+          <Img fluid={fluid[3]} />
+          <ExhibitionFilter />
+        </ExhibitionStaff>
+      </Exhibition>
+      <Wrapper>
+        <H2 en="SHUN" ja="なかだてしゅん" />
+      </Wrapper>
+      <Wrapper>
+        <P>
+          プロフィールや、沿革、自己紹介など。プロフィールや、沿革、自己紹介など。プロフィールや、沿革、自己紹介など。
+        </P>
+      </Wrapper>
+      <Br />
+      <Exhibition>
+        <ExhibitionStaff>
+          <Img fluid={fluid[4]} />
+          <ExhibitionFilter />
+        </ExhibitionStaff>
+      </Exhibition>
+      <Wrapper>
+        <H2 en="RISA" ja="あらきりさ" />
+      </Wrapper>
+      <Wrapper>
+        <P>
+          プロフィールや、沿革、自己紹介など。プロフィールや、沿革、自己紹介など。プロフィールや、沿革、自己紹介など。
+        </P>
       </Wrapper>
     </>
   )
@@ -97,6 +123,33 @@ const Salon: React.FC = () => {
 
 const P = styled.p`
   ${styles.mixins.lhCrop(2)}
+`
+const ExhibitionImg = styled.div`
+  position: relative;
+  width: 100%;
+  height: 105vw;
+  .gatsby-image-wrapper {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+const ExhibitionFilter = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(${functions.hex2rgb(styles.colors.light.shape)}, 0.3);
+`
+const ExhibitionStaff = styled.div`
+  position: relative;
+  width: 100%;
+  height: 52.5vw;
+  .gatsby-image-wrapper {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `
 
 export default Salon
