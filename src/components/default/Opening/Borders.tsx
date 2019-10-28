cb399d984540b14a060215c1cc78acd242f79bb1
soichiro-nitta <soic1226@gmail.com>
2019-10-28 10:34:32 +0900

    Remove borderIn module

--- src/components/default/Opening/Borders.tsx
+++ src/components/default/Opening/Borders.tsx
@@ -12,10 +12,10 @@ const Borders: React.FC = () => {
   const border4 = React.useRef()
   useEffectAsync(
     async () => {
-      animations.borderIn(border1.current)
-      animations.borderIn(border2.current)
-      animations.borderIn(border3.current)
-      animations.borderIn(border4.current)
+      animations.y(border1.current, '0%', 7, 'Out')
+      animations.y(border2.current, '0%', 7, 'Out')
+      animations.y(border3.current, '0%', 7, 'Out')
+      animations.y(border4.current, '0%', 7, 'Out')
       await functions.delay(3)
       animations.borderOutDown(border4.current)
       animations.borderOutUp(border3.current)
--- src/utils/animations/index.ts
+++ src/utils/animations/index.ts
@@ -1,9 +1,9 @@
 import path from '~/utils/animations/path'
 import paths from '~/utils/animations/paths'
-import borderIn from '~/utils/animations/borderIn'
 import borderOutUp from '~/utils/animations/borderOutUp'
 import borderOutDown from '~/utils/animations/borderOutDown'
 import set from '~/utils/animations/set'
+import y from '~/utils/animations/y'
 import scale from '~/utils/animations/scale'
 import scaleX from '~/utils/animations/scaleX'
 import opacity from '~/utils/animations/opacity'
@@ -11,10 +11,10 @@ import opacity from '~/utils/animations/opacity'
 export default {
   path,
   paths,
-  borderIn,
   borderOutUp,
   borderOutDown,
   set,
+  y,
   scale,
   scaleX,
   opacity
