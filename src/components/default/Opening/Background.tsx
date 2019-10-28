41a5f3af2ced9e4a1951ddf36c52d786c9bc6f79
soichiro-nitta <soic1226@gmail.com>
2019-10-28 09:57:15 +0900

    Add background component

---
+++ src/components/default/Opening/Background.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+import animations from '~/utils/animations'
+import functions from '~/utils/functions'
+import styles from '~/utils/styles'
+import useEffectAsync from '~/hooks/base/useEffectAsync'
+
+const Background: React.FC = () => {
+  const root = React.useRef()
+  useEffectAsync(
+    async () => {
+      await functions.delay(3.5)
+      animations.shapeOut(root.current)
+    },
+    { deps: [] }
+  )
+  return <Root ref={root} />
+}
+
+const Root = styled.div`
+  width: 100%;
+  height: 100%;
+  background: ${styles.colors.light.background};
+`
+
+export default Background
