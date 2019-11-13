6c89d3b835eb13e226ceb924682dd217ede134d9
soichiro-nitta <soic1226@gmail.com>
2019-11-13 19:42:21 +0900

    Add usePages hook

--- src/components/default/Pages/index.tsx
+++ src/components/default/Pages/index.tsx
@@ -1,48 +1,17 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
-// import functions from '~/utils/functions'
-// import useEffectAsync from '~/hooks/base/useEffectAsync'
+import functions from '~/utils/functions'
+import useEffectAsync from '~/hooks/base/useEffectAsync'
 import Page from '~/components/default/Pages/Page'
+import usePages from '~/hooks/default/Pages/usePages'
 
 type Props = {
   isPending: boolean
   setIsPending: (isPending: boolean) => void
 }
-type Pages = {
-  id: number
-  leave: boolean
-  node: React.ReactNode
-}[]
-type Action = {
-  type: 'init' | 'update'
-}
 
 const Pages: React.FC<Props> = props => {
-  const [pages, dispatch] = React.useReducer((pages: Pages, action: Action) => {
-    switch (action.type) {
-      case 'init':
-        return [{ id: 0, leave: false, node: props.children }]
-      case 'update': {
-        const modified = pages.map(value => {
-          return {
-            id: value.id,
-            leave: true,
-            node: value.node
-          }
-        })
-        return [
-          ...modified,
-          {
-            id: pages[pages.length - 1].id + 1,
-            leave: false,
-            node: props.children
-          }
-        ]
-      }
-      default:
-        throw new Error()
-    }
-  }, [])
+  const [pages, dispatch] = usePages(props.children)
   React.useEffect(() => {
     if (pages.length === 0) {
       dispatch({ type: 'init' })
@@ -54,24 +23,15 @@ const Pages: React.FC<Props> = props => {
       props.setIsPending(true)
     }
   }, [props.children])
-  // const cleanPages = (): void => {
-  //   console.log('cleanPages')
-  //   console.log(pages)
-  // setPages(
-  //   pages.filter(value => {
-  //     return !value.leave
-  //   })
-  // )
-  // }
-  // useEffectAsync({
-  //   effect: async () => {
-  //     if (!props.isPending) {
-  //       await functions.delay(1)
-  //       cleanPages()
-  //     }
-  //   },
-  //   deps: [props.isPending]
-  // })
+  useEffectAsync({
+    effect: async () => {
+      if (!props.isPending) {
+        await functions.delay(1)
+        dispatch({ type: 'clean' })
+      }
+    },
+    deps: [props.isPending]
+  })
   return (
     <Root id="main">
       {pages.map(value => {
---
+++ src/hooks/default/Pages/usePages.ts
@@ -0,0 +1,45 @@
+import * as React from 'react'
+
+type Pages = {
+  id: number
+  leave: boolean
+  node: React.ReactNode
+}[]
+type Action = {
+  type: 'init' | 'update' | 'clean'
+}
+
+const usePages = (node: React.ReactNode): [Pages, React.Dispatch<Action>] => {
+  return React.useReducer((pages: Pages, action: Action) => {
+    switch (action.type) {
+      case 'init':
+        return [{ id: 0, leave: false, node }]
+      case 'update': {
+        const modified = pages.map(value => {
+          return {
+            id: value.id,
+            leave: true,
+            node: value.node
+          }
+        })
+        return [
+          ...modified,
+          {
+            id: pages[pages.length - 1].id + 1,
+            leave: false,
+            node
+          }
+        ]
+      }
+      case 'clean': {
+        return pages.filter(value => {
+          return !value.leave
+        })
+      }
+      default:
+        throw new Error()
+    }
+  }, [])
+}
+
+export default usePages
