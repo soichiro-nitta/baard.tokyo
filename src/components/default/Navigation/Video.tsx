2af40029a7521729ad3fef2ee0c9f42864202068
soichiro-nitta <soic1226@gmail.com>
2019-11-30 23:48:27 +0900

    Remove: staticフォルダとGit LFS関連のファイルを除去

--- src/components/default/Navigation/Video.tsx
+++ src/components/default/Navigation/Video.tsx
@@ -14,7 +14,9 @@ type Props = {
 }
 
 const Video: React.FC<Props> = props => {
-  const src = config.dev ? `${props.src}` : `${config.firebase}/${props.src}`
+  const src = config.dev
+    ? `${config.local}/${props.src}`
+    : `${config.firebase}/${props.src}`
   const video = React.useRef<HTMLVideoElement>(null)
   useLoad({ video })
   usePlaying({ playing: props.playing, gnav: props.gnav })
