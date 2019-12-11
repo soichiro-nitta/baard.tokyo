26c241cf0328a172dd68d49b63ca2be5af1a8dd1
soichiro-nitta <soic1226@gmail.com>
2019-12-12 08:14:55 +0900

    Remove: gatsby-plugin-sassを除去

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
     `gatsby-plugin-typescript`,
-    `gatsby-plugin-sass`,
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
