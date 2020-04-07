3e637e2a482787ea35585f3c9c1dc3db6b9bf066
soichiro-nitta <soic1226@gmail.com>
2020-04-07 16:43:08 +0900

    feat: Google Analyticsを追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,8 +25,13 @@ module.exports = {
           include: `${__dirname}/src/assets/svg/`
         }
       }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-162950070-1'
+      }
     }
-
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
   ]
